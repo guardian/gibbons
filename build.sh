1e3806d08fb4aeb4b3b21585dd30cf430d801d80
Regis Kuckaertz <regis_kuckaertz@33020.gnm.int>
2018-05-03 09:05:38 +0100

    Add TeamCity assets

---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+JAVA_CMD=java
+[ -n "$JDK_HOME" ] && JAVA_CMD=$JDK_HOME/bin/java
+echo "Using Java command: $JAVA_CMD"
+
+SBT_OPTIONS="-Xmx1G \
+    -XX:MaxPermSize=250m \
+    -XX:+UseCompressedOops \
+    -Dsbt.log.noformat=true \
+    -Dbuild.number=$BUILD_NUMBER \
+    -Dbuild.vcs.number=$BUILD_VCS_NUMBER"
+
+[ -d target ] && rm -rf target
+mkdir target
+cd $(dirname $0)/target
+mkdir -p artifacts/gibbons
+
+if cd .. && $JAVA_CMD $SBT_OPTIONS -jar ./sbt-launch.jar assembly && cd target
+then
+    cp scala-*/*.jar ./artifacts/gibbons/gibbons.jar
+else
+    echo 'Failed to build exact-target-lambda'
+    exit 1
+fi
+
+cp ../riff-raff.yaml ./artifacts
+
+echo "##teamcity[publishArtifacts '$(pwd)/artifacts => .']"
