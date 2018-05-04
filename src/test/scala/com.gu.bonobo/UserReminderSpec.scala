45481777d182c76e65821a7969325ba9d1234f6a
Regis Kuckaertz <regis_kuckaertz@33020.gnm.int>
2018-05-04 10:43:47 +0100

    Remove tests

--- src/test/scala/com.gu.bonobo/UserReminderSpec.scala
+++ src/test/scala/com.gu.bonobo/UserReminderSpec.scala
@@ -22,52 +22,14 @@ class IntegrationTests extends FlatSpec {
         EmailSettings("", "", Email("")),
     )
 
-    val iSettings = InteractionSettings(
-        Regions.fromName("eu-west-1"),
-        DynamoSettings(""),
-        DynamoSettings(""),
-        "",
-        ""
-    )
-
     val userReminder = new UserReminder(settings, emailService, bonoboService, loggingService)
     val userDidNotAnswer = new UserDidNotAnswer(settings, emailService, bonoboService, loggingService)
-    val userSaidYes = new UserSaidYes(iSettings, bonoboService, loggingService)
-    val userSaidNo = new UserSaidNo(iSettings, bonoboService, loggingService)
 
     "The Reminder service" should "send reminders, duh" in {
         val ((newKeys, _, emailService), sentEmails) = userReminder.run(todayInstant).run((keys, users, Set.empty)).value
         val remindedUsers = newKeys.filter(_._2.remindedOn.exists(_ == todayInstant)).map(_._2.userId).toSet
         assert(remindedUsers.forall(u => emailService.contains(users(u).email)))
         assert(sentEmails.length == emailService.size)
-    }
-
-    "The SaidNo service" should "delete the key" in {
-        val sampleKey = keys.keys.head
-        val (repos, res) = userSaidNo.run(sampleKey).run((keys, users, Set.empty)).value
-        assert(!repos._1.contains(sampleKey))
-        assert(res == Success)
-    }
-
-    "The SaidNo service" should "warn if the key does not exist" in {
-        val sampleKey = KeyId("blablabla")
-        val (repos, res) = userSaidNo.run(sampleKey).run((keys, users, Set.empty)).value
-        assert(repos._1 == keys)
-        assert(res == KeyNotFound(sampleKey))
-    }
-
-    "The SaidYes service" should "extend the key lifetime" in {
-        val sampleKey = keys.keys.head
-        val (repos, res) = userSaidYes.run(sampleKey).run((keys, users, Set.empty)).value
-        assert(!repos._1.get(sampleKey).exists(_.remindedOn == todayInstant))
-        assert(res == Success)
-    }
-
-    "The SaidYes service" should "warn if the key does not exist" in {
-        val sampleKey = KeyId("blablabla")
-        val (repos, res) = userSaidYes.run(sampleKey).run((keys, users, Set.empty)).value
-        assert(repos._1 == keys)
-        assert(res == KeyNotFound(sampleKey))
     }
 
     "The DidNotAnswer service" should "remove expired keys" in {
