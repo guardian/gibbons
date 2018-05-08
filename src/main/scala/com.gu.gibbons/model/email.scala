a33fd1ae7a9cd60660c63f2e0ac48a60f28041c7
Regis Kuckaertz <regis_kuckaertz@33020.gnm.int>
2018-05-08 10:54:37 +0100

    Remove Destination value object

--- src/main/scala/com.gu.gibbons/model/email.scala
+++ src/main/scala/com.gu.gibbons/model/email.scala
@@ -13,15 +13,3 @@ case class EmailResult(val msgId: String) extends AnyVal
 object EmailResult {
   implicit val encoder: Encoder[EmailResult] = deriveEncoder
 }
-
-/** An email destination
-  * 
-  * @param to A list of destination email addresses
-  * @param cc A list of carbon-copy email addressses
-  * @param bcc A list of blind carbon-copy email addresses
-  */
-case class Destination(
-    to: Email,
-    cc: Seq[Email] = Nil,
-    bcc: Seq[Email] = Nil
-)
