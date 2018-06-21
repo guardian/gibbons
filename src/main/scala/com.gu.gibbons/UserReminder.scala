48ef5775e36ad8b04904e6f1415b783ab186cccd
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2018-06-21 09:57:02 +0100

    Apply to both lambdas

--- src/main/scala/com.gu.gibbons/UserReminder.scala
+++ src/main/scala/com.gu.gibbons/UserReminder.scala
@@ -31,7 +31,7 @@ class UserReminder[F[_]: Monad](
     for {
       _ <- logger.info(s"Getting all the users older than ${Settings.inactivityPeriod}")
       users <- bonobo.getUsers(now.minus(Settings.inactivityPeriod).toInstant)
-      filteredUsers <- users.foldMapM(u => bonobo.isDeveloper(u).map(b => if (b) Vector(u) else Vector.empty))
+      filteredUsers <- bonobo.isDeveloper(users)
     } yield filteredUsers
 
   def processUser(now: OffsetDateTime)(user: User): F[(UserId, Option[EmailResult])] = {
