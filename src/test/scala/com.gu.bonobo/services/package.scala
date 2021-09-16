7dfed2a2e702c52f1c5850e493536ca337652c4d
Rowanne Kabalan <rowanne.kabalan@guardian.co.uk>
2021-09-16 10:40:01 +0100

    Fix integration tests

--- src/test/scala/com.gu.bonobo/services/package.scala
+++ src/test/scala/com.gu.bonobo/services/package.scala
@@ -9,7 +9,7 @@ import model._
 package object services {
   type UserRepo = Map[UserId, User]
   type EmailRepo = Set[Email]
-  type KeyRepo = Set[Key]
+  type KeyRepo = Map[String, Key]
   type Repo = (UserRepo, EmailRepo, KeyRepo)
   type TestProgram[A] = State[Repo, A]
 }
@@ -18,18 +18,18 @@ package object fixtures {
     val today = OffsetDateTime.of(2018, 4, 25, 10, 15, 30, 0, ZoneOffset.UTC)
     val todayInstant = today.toInstant
 
-    val keys: Set[Key] = Set(
-        Key.create("user0", "fake-consumer-id", "Developer", "2012-04-25T10:15:30.00Z"),
-        Key.create("user1", "fake-consumer-id", "Developer", "2015-08-25T10:15:30.00Z", Some("2015-08-25T10:15:30.00Z")),
-        Key.create("user2", "fake-consumer-id", "Developer", "2012-04-25T10:15:30.00Z"),
-        Key.create("user3", "fake-consumer-id", "Developer", "2003-02-25T10:15:30.00Z", Some("2017-08-25T10:15:30.00Z"), Some("2018-04-01T10:15:30.00Z")),
-        Key.create("user4", "fake-consumer-id", "Developer", "2012-04-25T10:15:30.00Z"),
-        Key.create("user5", "fake-consumer-id", "Developer", "2012-04-25T10:15:30.00Z", Some("2015-08-25T10:15:30.00Z"), Some("2015-08-25T10:15:30.00Z")),
-        Key.create("user6", "fake-consumer-id", "Developer",  "2012-04-25T10:15:30.00Z", Some("2017-08-25T10:15:30.00Z")),
-        Key.create("user7", "fake-consumer-id", "Developer",  "2012-04-25T10:15:30.00Z", Some("2015-08-25T10:15:30.00Z"), Some("2018-04-25T10:15:30.00Z")),
-        Key.create("user8", "fake-consumer-id", "Developer", "2012-04-25T10:15:30.00Z"),
-        Key.create("user9", "fake-consumer-id", "Developer",  "2012-04-25T10:15:30.00Z", Some("2015-08-25T10:15:30.00Z"), Some("2018-04-01T10:15:30.00Z"))
-    )
+    val keys: Map[String, Key] = Map(Seq(
+        Key.create("user0", "consumerId0", "Developer", "2012-04-25T10:15:30.00Z"),
+        Key.create("user1", "consumerId1", "Developer", "2015-08-25T10:15:30.00Z", Some("2015-08-25T10:15:30.00Z")),
+        Key.create("user2", "consumerId2", "Developer", "2012-04-25T10:15:30.00Z"),
+        Key.create("user3", "consumerId3", "Developer", "2003-02-25T10:15:30.00Z", Some("2017-08-25T10:15:30.00Z"), Some("2018-04-01T10:15:30.00Z")),
+        Key.create("user4", "consumerId4", "Developer", "2012-04-25T10:15:30.00Z"),
+        Key.create("user5", "consumerId5", "Developer", "2012-04-25T10:15:30.00Z", Some("2015-08-25T10:15:30.00Z"), Some("2015-08-25T10:15:30.00Z")),
+        Key.create("user6", "consumerId6", "Developer",  "2012-04-25T10:15:30.00Z", Some("2017-08-25T10:15:30.00Z")),
+        Key.create("user7", "consumerId7", "Developer",  "2012-04-25T10:15:30.00Z", Some("2015-08-25T10:15:30.00Z"), Some("2018-04-25T10:15:30.00Z")),
+        Key.create("user8", "consumerId8", "Developer", "2012-04-25T10:15:30.00Z"),
+        Key.create("user9", "consumerId9", "Developer",  "2012-04-25T10:15:30.00Z", Some("2015-08-25T10:15:30.00Z"), Some("2018-04-01T10:15:30.00Z"))
+    ).map(key => key.consumerId -> key): _*)
 
     val users: Map[UserId, User] = Map(Seq(
         User.create("user0", "Florence Bowen", "florence.bowen@domain.com"),
