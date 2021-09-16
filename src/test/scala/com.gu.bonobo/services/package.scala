9ec1a613392ff4ef9d319138efbc498ff5607d8e
Rowanne Kabalan <rowanne.kabalan@guardian.co.uk>
2021-09-16 10:14:42 +0100

    Update test data

--- src/test/scala/com.gu.bonobo/services/package.scala
+++ src/test/scala/com.gu.bonobo/services/package.scala
@@ -19,28 +19,28 @@ package object fixtures {
     val todayInstant = today.toInstant
 
     val keys: Set[Key] = Set(
-        Key(UserId("user0"), "Developer"),
-        Key(UserId("user1"), "Developer"),
-        Key(UserId("user2"), "Developer"),
-        Key(UserId("user3"), "Developer"),
-        Key(UserId("user4"), "Developer"),
-        Key(UserId("user5"), "Developer"),
-        Key(UserId("user6"), "Developer"),
-        Key(UserId("user7"), "Developer"),
-        Key(UserId("user8"), "Developer"),
-        Key(UserId("user9"), "Developer")
+        Key.create("user0", "fake-consumer-id", "Developer", "2012-04-25T10:15:30.00Z"),
+        Key.create("user1", "fake-consumer-id", "Developer", "2015-08-25T10:15:30.00Z", Some("2015-08-25T10:15:30.00Z")),
+        Key.create("user2", "fake-consumer-id", "Developer", "2012-04-25T10:15:30.00Z"),
+        Key.create("user3", "fake-consumer-id", "Developer", "2003-02-25T10:15:30.00Z", Some("2017-08-25T10:15:30.00Z"), Some("2018-04-01T10:15:30.00Z")),
+        Key.create("user4", "fake-consumer-id", "Developer", "2012-04-25T10:15:30.00Z"),
+        Key.create("user5", "fake-consumer-id", "Developer", "2012-04-25T10:15:30.00Z", Some("2015-08-25T10:15:30.00Z"), Some("2015-08-25T10:15:30.00Z")),
+        Key.create("user6", "fake-consumer-id", "Developer",  "2012-04-25T10:15:30.00Z", Some("2017-08-25T10:15:30.00Z")),
+        Key.create("user7", "fake-consumer-id", "Developer",  "2012-04-25T10:15:30.00Z", Some("2015-08-25T10:15:30.00Z"), Some("2018-04-25T10:15:30.00Z")),
+        Key.create("user8", "fake-consumer-id", "Developer", "2012-04-25T10:15:30.00Z"),
+        Key.create("user9", "fake-consumer-id", "Developer",  "2012-04-25T10:15:30.00Z", Some("2015-08-25T10:15:30.00Z"), Some("2018-04-01T10:15:30.00Z"))
     )
 
     val users: Map[UserId, User] = Map(Seq(
-        User.create("user0", "Florence Bowen", "florence.bowen@domain.com", "2012-04-25T10:15:30.00Z"),
-        User.create("user1", "Margaret Woolley", "margaret.woolley@domain.com", "2015-08-25T10:15:30.00Z", Some("2015-08-25T10:15:30.00Z")),
-        User.create("user2", "Ruth Clay", "ruth.clay@domain.com", "2012-04-25T10:15:30.00Z"),
-        User.create("user3", "Anna Derrick", "anna.derrick@domain.com", "2003-02-25T10:15:30.00Z", Some("2017-08-25T10:15:30.00Z"), Some("2018-04-01T10:15:30.00Z")),
-        User.create("user4", "Frances Li", "frances.li@domain.com", "2012-04-25T10:15:30.00Z"),
-        User.create("user5", "Mildred Blundell", "mildred.blundell@domain.com", "2012-04-25T10:15:30.00Z", Some("2015-08-25T10:15:30.00Z"), Some("2015-08-25T10:15:30.00Z")),
-        User.create("user6", "Elizabeth Blaese", "elizabeth.blaese@domain.com", "2012-04-25T10:15:30.00Z", Some("2017-08-25T10:15:30.00Z")),
-        User.create("user7", "Marie Wolfe", "marie.wolfe@domain.com", "2012-04-25T10:15:30.00Z", Some("2015-08-25T10:15:30.00Z"), Some("2018-04-25T10:15:30.00Z")),
-        User.create("user8", "Dorothy Robbins", "dorothy.robbins@domain.com", "2012-04-25T10:15:30.00Z"),
-        User.create("user9", "Mary Allen", "mary.allen@domain.com", "2012-04-25T10:15:30.00Z", Some("2015-08-25T10:15:30.00Z"), Some("2018-04-01T10:15:30.00Z"))
+        User.create("user0", "Florence Bowen", "florence.bowen@domain.com"),
+        User.create("user1", "Margaret Woolley", "margaret.woolley@domain.com"),
+        User.create("user2", "Ruth Clay", "ruth.clay@domain.com"),
+        User.create("user3", "Anna Derrick", "anna.derrick@domain.com"),
+        User.create("user4", "Frances Li", "frances.li@domain.com"),
+        User.create("user5", "Mildred Blundell", "mildred.blundell@domain.com"),
+        User.create("user6", "Elizabeth Blaese", "elizabeth.blaese@domain.com"),
+        User.create("user7", "Marie Wolfe", "marie.wolfe@domain.com"),
+        User.create("user8", "Dorothy Robbins", "dorothy.robbins@domain.com"),
+        User.create("user9", "Mary Allen", "mary.allen@domain.com")
     ).map(user => user.id -> user): _*)
 }
